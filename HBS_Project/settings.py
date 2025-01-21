0c5bb155d81b45238980265366f6bc6e7d54d25f
unknown <kalai73579@gmail.com>
2025-01-21 20:00:08 +0530

    First commit

--- HBS_Project/settings.py
+++ HBS_Project/settings.py
@@ -246,6 +246,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://hbs-admin-afcea2f2324b.herokuapp.com',
     'https://new-hbs-admin-82beda5bc10a.herokuapp.com',
     'chrome-extension://eejfoncpjfgmeleakejdcanedmefagga',
+    'https://recychbs-app-c05d5f684be1.herokuapp.com',
 
     # 'https://demo-djreact-recyc-app-ee540343796a.herokuapp.com'
 
@@ -263,6 +264,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://hsb-ui.onrender.com',
     'https://hbs-admin-afcea2f2324b.herokuapp.com',
     'https://new-hbs-admin-82beda5bc10a.herokuapp.com',
+    'https://recychbs-app-c05d5f684be1.herokuapp.com'
+
     # 'https://demo-djreact-recyc-app-ee540343796a.herokuapp.com'
 
 ]
