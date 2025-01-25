172c8234928f39cb1ee446cff5ae78e033eb30bc
santhosh452022 <santhoshwwep2005@gmail.com>
2025-01-25 14:49:38 +0530

    First commit

--- src/screen/OtpVerification.jsx
+++ src/screen/OtpVerification.jsx
@@ -118,7 +118,7 @@ const OtpVerification = () => {
     e.preventDefault();
     setTimer(30);
     setIsTimerActive(true);
-    fetch("https://recychbs-app-c05d5f684be1.herokuapp.com/resend-otp/", {
+    fetch("resend-otp/", {
       credentials: "include",
       method: "POST",
       headers: {
@@ -147,7 +147,7 @@ const OtpVerification = () => {
     const enteredOtp = otp.join("");
     console.log("Otp Button Clicked");
     setLoading(true);
-    fetch("https://recychbs-app-c05d5f684be1.herokuapp.com/otp/", {
+    fetch("otp/", {
       method: "POST",
       credentials: "include",
       headers: {
